5ad869d7010d91d198f45356ee138259192e882b
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2024-02-27 11:26:23 +0700

    init project

---
+++ TokTokFake/src/main/java/com/example/TokTokFake/TokTokFakeApplication.java
@@ -0,0 +1,13 @@
+package com.example.TokTokFake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokTokFakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokTokFakeApplication.class, args);
+	}
+
+}
---
+++ TokTokFake/src/test/java/com/example/TokTokFake/TokTokFakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TokTokFake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokTokFakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
